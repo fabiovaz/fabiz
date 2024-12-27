6a1bbed6537613c54ad37a7f79d1d0f7d3975b60
Fabio Vaz <fabiovaz@gmail.com>
2024-12-27 19:19:54 +0000

    fix: remove appvue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,19 +14,24 @@ export default defineNuxtConfig({
       }
     }
   },
+
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   modules: [['@nuxtjs/google-fonts', {
     families: {
       'Inter': [300, 600]
     }
   }], ['nuxt-gtag', {
     id: 'G-SHG6DD2QW2'
-  }], '@nuxtjs/google-fonts', 'nuxt-gtag']
+  }], '@nuxtjs/google-fonts', 'nuxt-gtag'],
+
+  compatibilityDate: '2024-12-27'
 })
